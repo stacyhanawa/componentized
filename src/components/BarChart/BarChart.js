6d515f9bc3c3b9dc13fbddf1bb631fc6b6d43358
stacyhanawa <stacyhanawa@gmail.com>
2019-05-04 07:08:14 -0700

    created initial directory structure with component files

---
+++ src/components/BarChart/BarChart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './BarChart.css';
+
+class BarChart extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default BarChart;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+export default Nav;
