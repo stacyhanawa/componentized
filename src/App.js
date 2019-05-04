47e5a09de0c8c4f2d9ba14142f8e5938926ebfcb
stacyhanawa <stacyhanawa@gmail.com>
2019-05-04 06:53:18 -0700

    copied over App.js and App.css from react_chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    apiData: '',
+    currencies: [
+        "EUR",
+        "USD",
+        "AUD",
+        "GBP",
+        "BRL",
+    ],
+    rates: [
+    ],
+    selectedCurrency: "EUR",
+    highest: 0,
+  }
+  
+
+  handleChange = (event) => {
+    console.log(event.target.value);
+    this.state.selectedCurrency = event.target.value;
+    this.doFetch();
+  }
+  
+  doFetch = () => {
+    fetch("https://api.exchangeratesapi.io/latest?base=" + this.state.selectedCurrency)
+    .then(response => response.json())
+    .then(data => {    
+
+        this.setState({
+            apiData: data,
+        });
+        
+        
+        this.state.highest = 0;
+        for (let currency of this.state.currencies) {
+            let rate = data.rates[currency]   
+            if (rate > this.state.highest) {
+            this.state.highest = rate;
+            }
+        }
+        
+        if (this.state.selectedCurrency === "EUR") {
+            data.rates.EUR = 1.00;
+        }
+        
+        
+        let rates = [];
+        for (let currency of this.state.currencies) {
+
+            let height = data.rates[currency]/this.state.highest * 100;
+            let item =  {
+                    currency: currency,
+                    height: height,
+                    rate: data.rates[currency],
+                };
+                  
+            rates.push(item);
+        }
+        
+        this.setState({
+            rates: rates
+        }); 
+    });
+  }
+  
+  
+  componentDidMount() {
+    this.doFetch();
+  }
+  
+  render() {
+    return (
+      <div className="App">
+                
+        <section className="TitleBar">
+            <h1>Currency</h1>
+            
+            <div className="CurrencyChooser">
+                <label>Base currency:
+                    <select className="CurrencyChooser-select"  onChange={this.handleChange}>
+                         {
+                            this.state.currencies.map(currency => (
+                                <option value={currency}>{currency}</option>
+                            ))
+                         }
+                    </select>
+                </label>
+            </div>
+        </section>
+        <section className="MainContainer">
+            <div className="CurrencyCheckboxList">
+            </div>
+            <div className="BarChart">
+                {
+                this.state.rates.map(rate => (
+                  <div 
+                    className="BarChart-bar" 
+                    style={{ height: rate.height + "%"}}>
+                    {rate.currency} {rate.rate.toFixed(2)}
+                  </div>
+                ))
+              }
+
+            </div>
+        </section>
+        
+      </div>      
+    );
+  }
 }
 
 export default App;
